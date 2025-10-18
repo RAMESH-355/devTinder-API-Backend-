c61efafd902c9774a6aad1c7517d7e8e7a8d7be5
RAMESH <rmshvalle@gmail.com>
2025-10-18 16:16:34 +0530

    Ready for deployment

--- src/Config/database.js
+++ src/Config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://rmshvalle_db_user:kw3EGoxrmruh7Gm9@cluster0.qlkk1qr.mongodb.net/devTinder", { useNewUrlParser: true, useUnifiedTopology: true }
+        "mongodb+srv://rmshvalle_db_user:kw3EGoxrmruh7Gm9@cluster0.qlkk1qr.mongodb.net/devTinder"
     );
 };
 
